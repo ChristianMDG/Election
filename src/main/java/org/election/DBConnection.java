60b0127291d09b0dad44724c02d61ce3d5850ec1
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-02-19 16:17:08 +0300

    chore(database) : finish config database

---
+++ src/main/java/org/election/DBConnection.java
@@ -0,0 +1,22 @@
+package org.election;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/electiondb";
+    private static final String USER = "christian";
+    private static final String PASSWORD = "092MLBOA";
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
