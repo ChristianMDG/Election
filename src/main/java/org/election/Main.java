01efce01792c2754fbd62c9e2b01a07479e8d091
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-02-23 22:39:14 +0300

    feat(findWinner) : rend l'affichage plus ergonomique

--- src/main/java/org/election/Main.java
+++ src/main/java/org/election/Main.java
@@ -1,14 +1,50 @@
 package org.election;
 
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-    DataRetriever dataRetriever = new DataRetriever();
-        System.out.println("totalVote="+dataRetriever.countAllVotes());
-        System.out.println(dataRetriever.countVotesByType());
-        System.out.println(dataRetriever.countValidVotesByCandidate());
-        System.out.println(dataRetriever.computeVoteSummary());
-        System.out.println("taux de participations = " +dataRetriever.computeTurnoutRate()+"%");
-        System.out.println(dataRetriever.findWinner());
+        DataRetriever dataRetriever = new DataRetriever();
+
+
+        int totalVote = Math.toIntExact(dataRetriever.countAllVotes());
+        System.out.println("------ TOTAL DES VOTES ------");
+        System.out.println("Total des votes : " + totalVote);
+        System.out.println();
+
+
+        System.out.println("===== VOTES PAR TYPE =====");
+        List<VoteTypeCount> votesByType = dataRetriever.countVotesByType();
+        for (VoteTypeCount vtc : votesByType) {
+            System.out.println(vtc.getVoteType() + " : " + vtc.getCount());
+        }
+        System.out.println();
+
+
+        System.out.println("----- VOTES VALIDES PAR CANDIDAT -----");
+        List<CandidateVoteCount> votesByCandidate = dataRetriever.countValidVotesByCandidate();
+        for (CandidateVoteCount cvc : votesByCandidate) {
+            System.out.println(cvc.getCandidateName() + " : " + cvc.getValidVoteCount());
+        }
+        System.out.println();
+
+
+        System.out.println("===== RESUME DES VOTES =====");
+        VoteSummary summary = dataRetriever.computeVoteSummary();
+        System.out.println("VALID : " + summary.getValidCount());
+        System.out.println("BLANK : " + summary.getBlankCount());
+        System.out.println("NULL  : " + summary.getNullCount());
+        System.out.println();
+
+        double turnoutRate = dataRetriever.computeTurnoutRate();
+        System.out.println("----- TAUX DE PARTICIPATION---------");
+        System.out.printf("Taux de participation : %.2f%%\n", turnoutRate);
+        System.out.println();
+
+
+        System.out.println("------- RESULTAT DE L'ELECTION -------");
+        ElectionResult winner = dataRetriever.findWinner();
+        System.out.println("Gagnant : " + winner.getCandidateName()
+                + " avec " + winner.getValidVoteCount() + " votes VALID");
     }
 }
